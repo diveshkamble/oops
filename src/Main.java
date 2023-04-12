8648a30dc5cc1809a46e8082df9bc0d2b3a59782
Divesh Kamble <diveshkamble@gmail.com>
2023-04-12 12:52:21 +0530

    Getter and Setter

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setMake("Maruti");
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        car.describeCar();
+    }
+}
